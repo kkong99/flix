3dd62805cb43e32f400ca38a883da839fd43dba1
Kimora Kong <kekong99@fb.com>
2018-06-27 11:10:05 -0700

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by Kimora Kong on 6/27/18.
+//  Copyright © 2018 Kimora Kong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Flix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flix
+//
+//  Created by Kimora Kong on 6/27/18.
+//  Copyright © 2018 Kimora Kong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
